49097ad5498a986df65fd57a41600d9380a96508
Thomas.Ellyatt <thomas.ellyatt@gmail.com>
2023-01-30 21:32:20 +0000

    Added zip functionality

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import streamlit as st
 import pdf2image
 import zipfile
-import os
+import io
+
+st.set_page_config(
+    page_title="PDF to PNG",
+    layout="wide"
+)
+st.markdown("# :spiral_note_pad: Tom's :blue[PDF] to :red[PNG] Converter :frame_with_picture:")
+st.markdown("---")
 
 pdf_uploaded = st.file_uploader("Select a file", type="pdf")
-button = st.button("Confirm")
-image_down = []
-if button and pdf_uploaded is not None:
+top_container = st.empty()
 
+col1, col2, col3 = st.columns([1, 1, 4])
+with top_container, col1:
+    button = st.button("Confirm", type='primary')
+
+st.markdown("---")
+if button and pdf_uploaded is not None:
     if pdf_uploaded.type == "application/pdf":
         images = pdf2image.convert_from_bytes(pdf_uploaded.read(), poppler_path="poppler/library/bin")
-        for i, page in enumerate(images):
-            st.image(page, use_column_width=True)
-            st.download_button("Download", data=page, file_name=f"Image_{i}.png")
-            image_down.append(page)
+        file = io.BytesIO()
+        with zipfile.ZipFile(file, 'w') as z:
+            for i, page in enumerate(images):
+                st.image(page, use_column_width=True)
+                img = page
+                buf = io.BytesIO()
+                img.save(buf, format="png")
+                byte_im = buf.getvalue()
+                z.writestr(f'image_{i}.png', byte_im)
+        z.close()
+        file.seek(0)
+        zip_contents = file.getvalue()
+        with top_container, col2:
+            download = st.download_button("Download", data=zip_contents, file_name=f"Images.zip")
+
+        if download:
+            st.cache()
